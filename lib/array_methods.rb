08b1c08badee518ea778b8a10c5e8128aba827ca
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-09 16:38:54 +0000

    Automatically backed up by Learn

---
+++ lib/array_methods.rb
@@ -0,0 +1,24 @@
+def using_include(array, element)
+  using_include = ["array","element"]
+  array.include?(element)
+  end
+
+def using_sort(array)
+  using_sort = ["wow"] 
+  end
+
+def using_reverse(array)
+
+end
+
+def using_first(array)
+
+end
+
+def using_last(array)
+
+end
+
+def using_size(array)
+
+end
